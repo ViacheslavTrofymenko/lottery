ceb1746c67985e5187e72f02edbe37a77588e70d
Viacheslav Trofymenko <trofymenko.v.n@gmail.com>
2022-04-04 21:08:44 +0300

    Deployed contract to Ropsten testNetwork

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
-require('solidity-coverage')
+require('solidity-coverage');
+require('dotenv').config();
 
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -13,4 +16,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "ropsten",
+   networks: {
+      hardhat: {},
+      ropsten: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
 };
